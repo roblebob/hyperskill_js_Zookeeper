212569994a8d90ac80f05b9e89fbf52a28af9279
roblebob <robert.koppisch@gmail.com>
2024-07-28 19:51:05 +0200

    finish 1/4

---
+++ Zookeeper (JavaScript)/task/main.js
@@ -0,0 +1,6 @@
+const text = 'I love animals!\n' +
+    'Let\'s check on the animals...\n' +
+    'The deer looks fine.\n' +
+    'The bat looks happy.\n' +
+    'The lion looks healthy.'
+console.log(text)
